bc103dabd01c5d2136f8f8653627e5bfccd2ddfe
s1lencebtw <Vitalii.Letnianchyn.RI.2023@lpnu.ua>
2023-11-05 22:37:18 +0200

    Add project files.

---
+++ Lab 6.1 rec/Lab 6.1 rec.cpp
@@ -0,0 +1,81 @@
+﻿#include <iostream>
+#include <cmath>
+#include <random>
+#include <cstdlib>
+#include <ctime>
+#include <iomanip>
+using namespace std;
+
+void printArray(int* arr, int size, int i);
+void generateArray(int* arr, int size, int min, int max, int i);
+int countElements(int* arr, int size, int index, int count);
+int sumElements(int* arr, int size, int index, int sum);
+void replaceElements(int* arr, int size, int index);
+
+int main() {
+	const int arrSize = 25;
+	int t[arrSize];
+
+	generateArray(t, arrSize, 5, 90, 0);
+
+	cout << "Original array: ";
+	printArray(t, arrSize, 0);
+
+
+	cout << "Number of elemnts: " << countElements(t, arrSize, 0, 0) << endl;
+
+	cout << "Sum of elements: " << sumElements(t, arrSize, 0, 0) << endl;
+
+	replaceElements(t, arrSize, 0);
+
+	cout << "Modified:       ";
+	printArray(t, arrSize, 0);
+
+	return 0;
+}
+
+void generateArray(int* arr, int size, int min, int max, int i) {
+	if (i < size) {
+		arr[i] = min + rand() % (max - min + 1);
+		generateArray(arr, size, min, max, i + 1);
+	}
+}
+
+void printArray(int* array, int size, int i) {
+	cout << setw(4) << array[i] << " ";
+	if (i < size - 1)
+		printArray(array, size, i + 1);
+	else cout << endl;
+}
+
+
+int sumElements(int* arr, int size, int index, int sum) {
+	if (index < size) {
+		if (arr[index] % 2 == 0 || (index % 8 != 0))
+		{
+			sum += arr[index];
+		}
+		sumElements(arr, size, index + 1, sum);
+	}
+	else return sum;
+}
+
+int countElements(int* arr, int size, int index, int count) {
+	if (index < size) {
+		if (arr[index] % 2 == 0 || (index % 8 != 0)) {
+			count++;
+		}
+		countElements(arr, size, index + 1, count);
+	}
+	else return count;
+}
+
+void replaceElements(int* arr, int size, int index) {
+	if (index < size) {
+		if (arr[index] % 2 == 0 || (index % 8 != 0)) {
+			arr[index] = 0;
+		}
+		replaceElements(arr, size, index + 1);
+	}
+	else return;
+}
---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,25 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Lab 6.1 rec/Lab 6.1 rec.cpp"
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+
+		TEST_METHOD(TestMethod1)
+		{
+			int a[25] = { 87, 31, 6, 64, 47, 49, 57, 30, 73, 22, 5, 42, 42, 20, 21, 27, 6, 84, 16, 68, 55, 64, 53, 29, 33 };
+			int c = countElements(a, 25, 0, 0);
+			Assert::AreEqual(c, 22);
+		}
+		TEST_METHOD(TestMethod2)
+		{
+			int a[25] = { 87, 31, 6, 64, 47, 49, 57, 30, 73, 22, 5, 42, 42, 20, 21, 27, 6, 84, 16, 68, 55, 64, 53, 29, 33 };
+			int t = sumElements(a, 25, 0, 0);
+			Assert::AreEqual(t, 838);
+		}
+	};
+}
